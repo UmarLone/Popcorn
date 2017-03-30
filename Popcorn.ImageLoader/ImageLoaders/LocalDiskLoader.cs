0aa413824cf9facf782c049bd10d657562770530
Benjamin Bougot <benjamin@blacknut.com>
2017-03-30 15:07:20 +0200

    Improve image handling

---
+++ Popcorn.ImageLoader/ImageLoaders/LocalDiskLoader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace Popcorn.ImageLoader.ImageLoaders
+{
+    internal class LocalDiskLoader: ILoader
+    {
+
+        public Stream Load(string source)
+        {
+            //Thread.Sleep(1000);
+            return File.OpenRead(source);
+        }
+    }
+}
