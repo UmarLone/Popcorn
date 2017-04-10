f26365a78a89f35943a9d5fa66cadc6cd617c4aa
Benjamin Bougot <benjamin@blacknut.com>
2017-04-10 10:47:10 +0200

    Improve SplitView UI

--- Popcorn/UserControls/Home/Movie/MovieUserControl.xaml.cs
+++ Popcorn/UserControls/Home/Movie/MovieUserControl.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System.Windows;
-using System.Windows.Input;
-using Popcorn.Helpers;
 
 namespace Popcorn.UserControls.Home.Movie
 {
@@ -12,21 +10,11 @@ namespace Popcorn.UserControls.Home.Movie
         public MovieUserControl()
         {
             InitializeComponent();
-            Loaded += MovieUserControl_Loaded;
         }
 
-        private void MovieUserControl_Loaded(object sender, RoutedEventArgs e)
+        private void HamburgerButtonOnClick(object sender, RoutedEventArgs e)
         {
-            var window = System.Windows.Window.GetWindow(this);
-            if (window == null) return;
-
-            window.PreviewMouseDown += OnPreviewMouseDown;
-        }
-
-        private void OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            var element = e.OriginalSource as FrameworkElement;
-            SplitView.IsPaneOpen = element?.FindParentElement("Pane") != null;
+            SplitView.IsPaneOpen = !SplitView.IsPaneOpen;
         }
     }
 }
